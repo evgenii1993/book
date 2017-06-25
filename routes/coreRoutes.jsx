b35b8b60b92f08d038d79896b7c2414da45d4d6b
Mironov Oleg <mironovpib101@gmail.com>
2017-06-25 13:55:38 +0400

    fix route server and client

--- routes/coreRoutes.jsx
+++ routes/coreRoutes.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import {StaticRouter, Switch, Route} from 'react-router';
+//import { Switch, Route } from 'react-router';
 import AppRoot from '../site/app';
-
+import Test from '../site/components/test/Test';
 
 module.exports = function(app) {
-    // let App = () => (
-    //         <Switch>
-    //             <Route path="/test" component={AppRoot}/>
-    //             <Route path="/" component={AppRoot}/>
-    //         </Switch>
-    //     );
+    let App = () => (
+            <Switch>
+                <Route path="/about" component={Test} type='app'/>
+                <Route path="/" component={AppRoot} type='app' name="app"/>
+            </Switch>
+        );
 
     app.get('*', (req, res) => {
         let context = {};
+        // const html = ReactDOMServer.renderToString(
+        //     <StaticRouter location={req.url} context={context}>
+        //         <AppRoot/>
+        //     </StaticRouter>
+        // );
         const html = ReactDOMServer.renderToString(
-            <StaticRouter location={req.url} context={context}>
-                <AppRoot/>
-            </StaticRouter>
+                <StaticRouter location={req.url} context={context}>
+                    <App/>
+                </StaticRouter>
         );
         res.render('index.ejs', {reactOutput: html});
     });
